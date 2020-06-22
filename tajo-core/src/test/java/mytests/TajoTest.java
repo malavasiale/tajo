a76554665c2943bb296ab2c60754cd0c425779d5
Live session user <malavasialessio@gmail.com>
2020-06-22 17:54:03 +0200

    jacoco integration

---
+++ tajo-core/src/test/java/mytests/TajoTest.java
@@ -0,0 +1,16 @@
+package mytests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TajoTest {
+
+	@Test
+	public void test() {
+		boolean t = true;
+		assertTrue(t);
+	}
+
+	
+}
