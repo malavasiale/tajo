2131d9f73b1aeb76916560bb95a746d459be0e1a
Live session user <malavasialessio@gmail.com>
2020-06-24 11:23:58 +0200

    task order tests

--- tajo-core/src/test/java/mytests/TaskComparatorTest.java
+++ tajo-core/src/test/java/mytests/TaskComparatorTest.java
@@ -3,6 +3,7 @@ package mytests;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.tajo.ExecutionBlockId;
@@ -11,6 +12,7 @@ import org.apache.tajo.conf.TajoConf;
 import org.apache.tajo.master.event.TaskAttemptToSchedulerEvent;
 import org.apache.tajo.util.JSPUtil;
 import org.apache.tajo.util.TajoIdUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -25,45 +27,57 @@ import org.apache.tajo.querymaster.Task;
 @RunWith(JUnitParamsRunner.class)
 public class TaskComparatorTest {
 	
+	List<Task> toOrder;
+	Configuration conf;
+	TaskAttemptToSchedulerEvent.TaskAttemptScheduleContext scheduleContext;
+	ExecutionBlockId ebId;
 	
-	@Test
+	@Before
 	public void prepareTasks() {
-		List<Task> toOrder = new ArrayList<>();
-	    Configuration conf = new TajoConf();
+		toOrder = new ArrayList<>();
+	    conf = new TajoConf();
 
-	    TaskAttemptToSchedulerEvent.TaskAttemptScheduleContext scheduleContext =
-	        new TaskAttemptToSchedulerEvent.TaskAttemptScheduleContext();
+	    scheduleContext = new TaskAttemptToSchedulerEvent.TaskAttemptScheduleContext();
 
-	    ExecutionBlockId ebId = TajoIdUtils.createExecutionBlockId("eb_000001_00001_00001");
-	    
-	    TaskId id = new TaskId(ebId, 1);
+	    ebId = TajoIdUtils.createExecutionBlockId("eb_000001_00001_00001");
+	}
+	
+	public Task createTask(int taskId,int launchTime,int finishTime) {
+		TaskId id = new TaskId(ebId, taskId);
 	    Task task = new Task(conf, scheduleContext, id, true, null);
 	    
-	    task.setLaunchTime(1);
-        task.setFinishTime(3);
+	    task.setLaunchTime(launchTime);
+        task.setFinishTime(finishTime);
         
-        toOrder.add(task);
-        
-        id = new TaskId(ebId, 3);
-	    task = new Task(conf, scheduleContext, id, true, null);
-	    
-	    task.setLaunchTime(3);
-        task.setFinishTime(5);
-        
-        toOrder.add(task);
-        
-        id = new TaskId(ebId, 2);
-	    task = new Task(conf, scheduleContext, id, true, null);
-	    
-	    task.setLaunchTime(2);
-        task.setFinishTime(4);
-        
-        toOrder.add(task);
-        
-        JSPUtil.sortTasks(toOrder, "id", "desc");
-        assertEquals(3,toOrder.get(0).getId().getId());
-    
-
+        return task;
 	}
+	
+	/*
+	 * ----------START TEST SORT TASK ---------------------*
+	 * Test del sort tramite id del metodo sortTask()
+	 * Category Partition:
+	 * List<Task> tasks : {valid, empty , null}
+	 * String order : {desc , asc , invalid, null}
+	 * String field : {id , host , runTime, startTime, invalid, null}
+	 * */
+	@Test
+	@Parameters({
+		"3,1,2,desc,3", // tasks = valid ; order = desc ; field = id
+		"3,1,2,asc,1", // tasks = valid ; order = asc ; field = id
+		"3,1,2,,3" // tasks = valid ; order = invalid ; field = id
+	})
+	public void sortTaskIdTest(int task1,int task2,int task3,String order,int expected) {
+		List<Integer> tasksID = Arrays.asList(task1,task2,task3);
+		for(Integer i : tasksID) {
+			Task t = createTask(i,1,2);
+			toOrder.add(t);
+		}
+		JSPUtil.sortTasks(toOrder, "id", order);
+		assertEquals(expected,toOrder.get(0).getId().getId());
+	}
+	
+	
+	
+	/*---------END TEST SORT TASK----------*/
 
 }
