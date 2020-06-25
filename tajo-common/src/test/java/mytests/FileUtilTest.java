daf5bdce61834367f8a955fa0158e6b0718af136
Live session user <malavasialessio@gmail.com>
2020-06-25 12:24:57 +0200

    first category partition

--- tajo-common/src/test/java/mytests/FileUtilTest.java
+++ tajo-common/src/test/java/mytests/FileUtilTest.java
@@ -3,21 +3,50 @@ package mytests;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.FileSystems;
+import java.io.PrintWriter;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.fs.Path;
 import org.apache.tajo.util.FileUtil;
+import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class FileUtilTest {
 
+	/*
+	 * Category Partition
+	 * String path {valid, notExsisting, invalid, null}
+	 * String textToWrite {len > 0 ; len = 0 ; null}
+	 * */
 	@Test
-	public void writeAndReadTest() throws IOException {
-		Path filepath  = new Path("test.txt");
-		FileUtil.writeTextToFile("prova", filepath);
-		String read = FileUtil.readTextFile(new File("test.txt"));
-		assertEquals("prova",read);
+	@Parameters({
+		"test.txt,prova", // path = valid ; textToWrite > 0
+		"test.txt,", // path = valid ; textToWrite = 0
+		"./newFolder/test.txt,prova" // path = noExisting ; textToWrite > 0
+	})
+	public void writeAndReadTest(String path,String textToWrite) throws IOException {
+		Path filepath  = new Path(path);
+		FileUtil.writeTextToFile(textToWrite, filepath);
+		String read = FileUtil.readTextFile(new File(path));
+		assertEquals(textToWrite,read);
+	}
+	
+	@After
+	public void clearFile() throws IOException {
+		try {
+			FileUtils.forceDelete(new File("test.txt"));
+		} catch(Exception e) {}
+		try {
+			FileUtils.deleteDirectory(new File("./newFolder"));
+		} catch (Exception e) {}
+		
 	}
 
 }
